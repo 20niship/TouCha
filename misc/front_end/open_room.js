820196df069af784572fde10c6042652ebaf6a53
KaiTakase <takasekai@i.softbank.jp>
2021-05-08 15:08:18 +0900

    オープンルーム一覧画面の大枠の見た目を作った

--- misc/front_end/open_room.js
+++ misc/front_end/open_room.js
@@ -26,27 +26,27 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 //         justifyContent: 'space-around',
 //         alignItems: 'center'}}
 //     >
-//     <TouchableOpacity onPress={navigation.navigate("RoomArchive")} >
+//     <TouchableOpacity onPress={navigation.navigate("Open_room")} >
 //         <Image source={{uri:'https://emoji.slack-edge.com/T03MDNTCW/%25E3%2581%2584%25E3%2581%2584%25E8%25A9%25B1/0fe642881d40d176.gif'}} style={{width:30, height:30 }}/>
-//         <Text style={{textSize:footer_text_size}}>RoomArchive</Text>
+//         <Text style={{textSize:footer_text_size}}>Open room</Text>
 //     </TouchableOpacity>
 
 //     {/* <TouchableOpacity onPress={navigation.navigate("Room")} > */}
-//     <TouchableOpacity onPress={console.log("RoomArchive")} >
+//     <TouchableOpacity onPress={console.log("Open_room")} >
 
 //         <Image source={{uri:'https://emoji.slack-edge.com/T03MDNTCW/%25E3%2581%2584%25E3%2581%2584%25E8%25A9%25B1/0fe642881d40d176.gif'}} style={{width:30, height:30 }}/>
 //         <Text style={{textSize:footer_text_size}}>Room</Text>
 //     </TouchableOpacity>
 
 //     {/* <TouchableOpacity onPress={navigation.navigate("Settings")} > */}
-//     <TouchableOpacity onPress={console.log("RoomArchive")} >
+//     <TouchableOpacity onPress={console.log("Open_room")} >
 //         <Image source={{uri:'https://emoji.slack-edge.com/T03MDNTCW/%25E3%2581%2584%25E3%2581%2584%25E8%25A9%25B1/0fe642881d40d176.gif'}} style={{width:30, height:30 }}/>
 //         <Text style={{textSize:footer_text_size}}>Settings</Text>
 //     </TouchableOpacity>
 
 
 //     {/* <TouchableOpacity onPress={navigation.navigate("Profile")} > */}
-//     <TouchableOpacity onPress={console.log("RoomArchive")} >
+//     <TouchableOpacity onPress={console.log("Open_room")} >
 //         <Image source={{uri:'https://emoji.slack-edge.com/T03MDNTCW/%25E3%2581%2584%25E3%2581%2584%25E8%25A9%25B1/0fe642881d40d176.gif'}} style={{width:30, height:30 }}/>
 //         <Text style={{textSize:footer_text_size}}>Profile</Text>
 //     </TouchableOpacity>
@@ -60,45 +60,92 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 // 各画面配置（
 // --------------------------------------------------------------------
 
-class RoomArchive extends React.Component{
+class IconTest extends React.Component {
+  render() {
+      return (
+          <View>
+              <Icon
+                  name={this.props.icon}
+                  size={this.props.fontSize ? this.props.fontSize : 50}
+              />
+          </View>
+      );
+  }
+}
+
+class Open_room extends React.Component{
     render(){
       const { navigation } = this.props;
     const room_temp = [
-       {name:"room1", icon:"kb.png", lastmsg:"Hello room 1", status:"ok"},
-       {name:"room2", icon:"kb.png", lastmsg:"Hello room 2", status:"ok"},
-       {name:"room3", icon:"kb.png", lastmsg:"Hello room 3", status:"ok"},
-       {name:"room4", icon:"kb.png", lastmsg:"Hello room 4", status:"ok"},
-       {name:"room5", icon:"kb.png", lastmsg:"Hello room 5", status:"ok"},
+       {name:"room1", icon_name:"user-circle", lastmsg:"Hello room 1", status:"ok"},
+       {name:"room2", icon_name:"user-circle", lastmsg:"Hello room 2", status:"ok"},
+       {name:"room3", icon_name:"user-circle", lastmsg:"Hello room 3", status:"ok"},
+       {name:"room4", icon_name:"user-circle", lastmsg:"Hello room 4", status:"ok"},
+       {name:"room5", icon_name:"user-circle", lastmsg:"Hello room 5", status:"ok"},
+       {name:"room6", icon_name:"user-circle", lastmsg:"Hello room 5", status:"ok"},
+       {name:"room7", icon_name:"user-circle", lastmsg:"Hello room 5", status:"ok"},
+       {name:"room8", icon_name:"user-circle", lastmsg:"Hello room 5", status:"ok"},
+       {name:"room9", icon_name:"user-circle", lastmsg:"Hello room 5", status:"ok"},
+       {name:"room10", icon_name:"user-circle", lastmsg:"Hello room 5", status:"ok"},
+       
     ];
     var room_list_ui = [];
-
-   var room_inner_text_2 = {
+    
+    var room_inner_text_2 = {
       margin:0,
       paddingHorizontal: 10,
       paddingVertical: 5,
       flexDirection:'row',
       flex: 1,
-  };
-
+    };
+    
     room_temp.forEach(function(room, index){
-        room_list_ui.push(
+      room_list_ui.push(
         //特定のルームに移動する頃はできていないので調整必要
-          <TouchableOpacity onPress={() => { navigation.navigate("Room"); }} style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
-            <Image source={room.icon} style={{ width: 50, height: 50 }} />
+        <TouchableOpacity onPress={() => { navigation.navigate("Room"); }} style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+            <View style={{width:50, height:50, marginLeft:20,marginTop:20}}>
+            <IconTest icon={room.icon_name} fontSize={45}/>
+            </View>
             <View style={styles.room_inner_text_1_TD} >
-              <Text style={{ fontSize: 16 }}>{room.name}</Text>
-              <Text style={{ fontSize: 10 }}>{room.lastmsg}</Text>
+              <Text style={{ fontSize: 18 }}>{room.name}</Text>
+              <Text style={{ fontSize: 14, color:"darkgray" }}>{room.lastmsg}</Text>
             </View>
           </TouchableOpacity >
         )
-    })
-
-    return( 
-        <ScrollView ref={(ref) => { this.scrollView = ref }} style={styles.messages}>
-          {room_list_ui}
-        </ScrollView>);
+      })
+      
+      return( 
+        <View>
+          <View style={{flexDirection:"row", justifyContent:"space-between", marginTop:40}}>
+            <Text style={{fontSize:20,marginLeft:10}}>オープンルーム</Text>
+            <TouchableHighlight style={{marginRight:20}}>
+              <IconTest icon="search" fontSize={30} />
+            </TouchableHighlight>
+          </View>
+          <ScrollView ref={(ref) => { this.scrollView = ref }} style={styles.messages}>
+            {room_list_ui}
+          </ScrollView>
+        </View>
+      );
     }
-}
+  }
+
+  const styles = StyleSheet.create({
+    room_inner_text_1_TD: {
+      borderRadius: 5,
+      marginTop: 8,
+      marginRight: 10,
+      marginLeft: 10,
+      paddingHorizontal: 0,
+      paddingVertical: 8,
+      flexDirection:'column',
+      flex: 1
+    },
+
+    messages:{
+      backgroundColor:"white"
+    },
+  })
 
 class Room extends React.Component{
     render(){
@@ -139,8 +186,8 @@ class Profile extends React.Component{
    return (
      <View style={{flex:1}}>
      <NavigationContainer>
-       <Stack.Navigator initialRouteName="RoomArchive">
-         <Stack.Screen name="RoomArchive" component={RoomArchive} />
+       <Stack.Navigator initialRouteName="Open_oom">
+         <Stack.Screen name="Open_room" component={Open_room} />
          <Stack.Screen name="Room" component={MyRoom} />
          <Stack.Screen name="Settings" component={MySettings} />
          <Stack.Screen name="Profile" component={MyProfile} />
@@ -155,27 +202,12 @@ class Profile extends React.Component{
  export default App;
 */
 
-const styles = StyleSheet.create({
-  room_inner_text_1_TD: {
-    borderRadius: 5,
-    marginTop: 8,
-    marginRight: 10,
-    marginLeft: 10,
-    paddingHorizontal: 0,
-    paddingVertical: 0,
-    flexDirection:'column',
-    flex: 1
-  },
-})
-
-
 // https://reactnavigation.org/docs/material-bottom-tab-navigator
 
 const Tab = createMaterialBottomTabNavigator();
   
   
   // <Image source={{uri:'https://emoji.slack-edge.com/T03MDNTCW/%25E3%2581%2584%25E3%2581%2584%25E8%25A9%25B1/0fe642881d40d176.gif'}} style={{width:30, height:30 }}/>
-
 
 export default function App() {
 
@@ -188,11 +220,26 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Archive" component={RoomArchive} activeColor="#e91e63" options={bar_options} />
-        <Tab.Screen name="Room" component={Room} activeColor="#e91e63" options={bar_options} />
-        <Tab.Screen name="Settings" component={Settings} activeColor="#e91e63" options={bar_options} />
-        <Tab.Screen name="Profile" component={Profile} activeColor="#e91e63" options={bar_options} />
+      <Tab.Navigator
+        screenOptions={({route}) => ({tabBarIcon:({color,size}) => {
+          size=25
+          if (route.name === 'Open_room') {
+            var iconName="university"
+          } else if (route.name === 'Room') {
+            iconName = "comment"
+          } else if (route.name === 'Settings') {
+            iconName = "cog"
+          } else if (route.name === 'Profile') {
+            iconName = "user"
+          } 
+          return <IconTest icon={iconName} fontSize={size}/>;
+        },
+      })}
+      >
+        <Tab.Screen name="Open_room" component={Open_room} activeColor="#e91e63" />
+        <Tab.Screen name="Room" component={Room} activeColor="#e91e63"/>
+        <Tab.Screen name="Settings" component={Settings} activeColor="#e91e63"/>
+        <Tab.Screen name="Profile" component={Profile} activeColor="#e91e63" />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ misc/front_end/test.js
@@ -0,0 +1,353 @@
+import React, { Component } from 'react';
+import { Text, View, Image, StyleSheet, ScrollView, KeyboardAvoidingView, TextInput, TouchableHighlight, Keyboard } from 'react-native';
+import AutogrowInput from 'react-native-autogrow-input';
+import io from "socket.io-client";
+
+const socket = io('https://acbc591940e9.ngrok.io', {transports: ['websocket']} );
+
+// 一時的に画像を読み込む（テスト用のみ）
+import photo_nerv from './images/nerv.jpg'
+import photo_qb   from './images/qb.jpg'
+import photo_shunji from './images/shinji.jpg'
+// import photo_bell from "./images/bell.png"
+
+//used to make random-sized messages
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default class ChatView extends Component {
+  constructor(props) {
+    super(props);
+    var messages = [
+      { direction:'left', usrIconURL:photo_nerv, text:"いかりしんじ、エヴァに乗れ！！", reactions:"" }, 
+      { direction:'left', usrIconURL:photo_nerv, text:"でなければ、、帰れ！", reactions:"" }, 
+      { direction:'left', usrIconURL:photo_nerv, text:"やっはろーーーーーあああああああああああああああ", reactions:"" }, 
+      { direction:'right', usrIconURL:photo_qb, text:"わけがわからないよわけがわからないよわけがわからないよわけがわからないよわけがわからないよ", reactions:"" }, 
+      { direction:'left', usrIconURL:photo_nerv, text:"いかりしんじ、エヴァに乗れ！！", reactions:"" }, 
+      { direction:'right', usrIconURL:photo_shunji, text:"逃げちゃだめだ、逃げちゃだめだ、逃げちゃだめだ、逃げちゃだめだ、逃げちゃだめだ！！！！", reactions:"" },
+      { direction:'left', usrIconURL:photo_shunji, text:"リアクションサンプル：", reactions:"" }
+    ];
+    this.state = {
+      //bell:photo_bell,
+      noti:6,
+      roomName:"TouChaグル",
+      member:7,
+      messages: messages,
+      inputBarText: ''
+    }
+  }
+
+  static navigationOptions = {
+    title: 'Chat',
+  };
+
+  RoomScrolltoEndWrapper (e) {this.scrollView.scrollToEnd();}
+  UNSAFE_componentWillMount () {
+    // ンポーネントがマウント(配置)される直前に呼び出される
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.RoomScrolltoEndWrapper.bind(this));
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.RoomScrolltoEndWrapper.bind(this));
+  }
+
+  componentWillUnmount() { this.keyboardDidShowListener.remove(); this.keyboardDidHideListener.remove();}
+   
+  componentDidMount()   {setTimeout(function() {this.RoomScrolltoEndWrapper(this);}.bind(this)); } // アプリ起動時に最下部までスクロール
+  componentDidUpdate() { setTimeout(function() {this.RoomScrolltoEndWrapper(this);}.bind(this));  } //メッセージが追加されたときに最下部までスクロール
+
+  _sendMessage() {
+    this.state.messages.push({ direction:'right', usrIconURL:photo_qb,  text: this.state.inputBarText, reactions:"" });
+
+//    socket.emit('message', this.state.inputBarText);
+
+    this.setState({
+      messages: this.state.messages,
+      inputBarText: ''
+    });
+  }
+
+  _onChangeInputBarText(text) {
+    this.setState({
+      inputBarText: text
+    });
+  }
+
+  //This event fires way too often.
+  //We need to move the last message up if the input bar expands due to the user's new message exceeding the height of the box.
+  //We really only need to do anything when the height of the InputBar changes, but AutogrowInput can't tell us that.
+  //The real solution here is probably a fork of AutogrowInput that can provide this information.
+  _onInputSizeChange() {
+    setTimeout(function() {
+      this.scrollView.scrollToEnd({animated: false});
+    }.bind(this))
+  }
+
+  render() {
+    var messages = [];
+    var that = this;
+
+    socket.on('message', function(msg) {
+
+      console.log("ahaha" + msg);
+      const findresult = that.state.messages.some((u) => u.text === msg);
+      if (!findresult) {
+        that.state.messages.push({ direction:'left', usrIconURL:photo_qb,  text:msg, reactions:"" });
+        that.setState({
+        messages: that.state.messages,
+        inputBarText: ''
+        });
+      }
+      console.log("gehe");
+
+    });
+
+    /* if(flag === 1){
+      flag = 0;
+      this.state.messages.push({ direction:'left', usrIconURL:photo_qb,  text:m, reactions:"" });
+      console.log("gehe");
+    } */
+
+    this.state.messages.forEach(function(message, index) {
+      messages.push(
+          <MessageBubble key={index} direction={message.direction} text={message.text} userIconUrl={message.usrIconURL} reactions={message.reactions}/>
+        );
+    });
+
+    return (
+
+      <View style={styles.outer}>
+          <View style={styles.topBar}>
+          <TouchableHighlight>
+                <Text style={styles.backButton}>&lt; {this.state.noti}</Text>
+              </TouchableHighlight>
+              <Text style={styles.textTop}>{this.state.roomName}({this.state.member})</Text>
+              <TouchableHighlight>
+              <Image source={require("./images/bell.png")} style={styles.bell}/>
+              </TouchableHighlight>
+          </View>
+          <KeyboardAvoidingView
+              style={{ flex: 1 }}
+              behavior="position"
+              contentContainerStyle={{ flex: 1 }}
+            >
+
+          <ScrollView ref={(ref) => { this.scrollView = ref }} style={styles.messages}>
+            {messages}
+          </ScrollView>
+          <InputBar onSendPressed={() => this._sendMessage()} 
+                    onSizeChange={() => this._onInputSizeChange()}
+                    onChangeText={(text) => this._onChangeInputBarText(text)}
+                    text={this.state.inputBarText}/>
+          {/* <KeyboardSpacer/>              */}
+          </KeyboardAvoidingView>
+          <View style={styles.bottom}></View>
+      </View>
+    );
+  }
+}
+
+//The bubbles that appear on the left or the right for the messages.
+class MessageBubble extends Component {
+  render() {
+    var bubbleStyles = this.props.direction === 'left' ? [styles.messageBubble, styles.messageBubbleLeft] : [styles.messageBubble, styles.messageBubbleRight];
+    var bubbleTextStyle = this.props.direction === 'left' ? styles.messageBubbleTextLeft : styles.messageBubbleTextRight;
+
+    if (this.props.direction === 'left'){
+        return (
+          <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+              <Image source={this.props.userIconUrl} style={{ width: 50, height: 50 }} />
+              <View style={styles.messageBubbleTD}>
+                <View style={bubbleStyles}><Text style={bubbleTextStyle}>{this.props.text}</Text></View>
+                <Image source={{uri:"https://emoji.slack-edge.com/T01SUSMC3U7/iihanashi/21e4a4659a628f58.gif"}} style={{ width: 20, height: 20 }} />
+              </View>
+              <View style={{width: 70}}/> 
+            </View>
+        );
+    }else{
+      return (
+        <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+            <View style={{width: 70}}/> 
+              <View style={styles.messageBubbleTD}>
+                <View style={bubbleStyles}><Text style={bubbleTextStyle}>{this.props.text}</Text></View>
+                <Image source={{uri:"https://emoji.slack-edge.com/T01SUSMC3U7/iihanashi/21e4a4659a628f58.gif"}} style={{ width: 20, height: 20 }} />
+              </View>
+            <Image source={this.props.userIconUrl} style={{ width: 50, height: 50 }} />
+          </View>
+      );
+    }
+  }
+}
+
+//The bar at the bottom with a textbox and a send button.
+class InputBar extends Component {
+
+  //AutogrowInput doesn't change its size when the text is changed from the outside.
+  //Thus, when text is reset to zero, we'll call it's reset function which will take it back to the original size.
+  //Another possible solution here would be if InputBar kept the text as state and only reported it when the Send button
+  //was pressed. Then, resetInputText() could be called when the Send button is pressed. However, this limits the ability
+  //of the InputBar's text to be set from the outside.
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if(nextProps.text === '') {
+      this.autogrowInput.resetInputText();
+    }
+  }
+
+  render() {
+    return (
+      <>
+          <View style={styles.inputBar}>
+            <TouchableHighlight>
+              <Image source={require("./images/plus.png")} style={styles.plus}/>
+            </TouchableHighlight>
+            <AutogrowInput style={styles.textBox}
+                        ref={(ref) => { this.autogrowInput = ref }} 
+                        multiline={true}
+                        defaultHeight={30}
+                        onChangeText={(text) => this.props.onChangeText(text)}
+                        onContentSizeChange={this.props.onSizeChange}
+                        value={this.props.text}/>
+            <TouchableHighlight style={styles.sendButton} onPress={() =>
+            {     
+              socket.emit('message', this.props.text);
+              this.props.onSendPressed();
+            }
+            }>
+                <Image source={require("./images/submit_arrow.png")} style={styles.arrow}/>
+            </TouchableHighlight>
+          </View> 
+
+          </>
+          );
+  }
+}
+
+//TODO: separate these out. This is what happens when you're in a hurry!
+const styles = StyleSheet.create({
+
+  //Top of Chat
+  topBar:{
+    marginTop:0,
+    paddingTop:40,
+    zIndex:1,
+    backgroundColor:"rgba(256,256,256,0.95)",
+    flexDirection: 'row',
+    justifyContent:"space-between",
+    alignItems:"center",
+  },
+
+  backButton:{
+    marginLeft:10,
+    fontSize:23,
+    margin:4,
+  },
+
+  textTop:{
+    fontSize:20,
+  },
+
+  bell:{
+    marginRight:20,
+    width:25,
+    height:25,
+  },
+
+  //ChatView
+  
+  outer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: 'white'
+  },
+
+  messages: {
+    flex: 1,
+  },
+
+  //InputBar of Chat
+
+  inputBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 5,
+    paddingTop:5,
+    paddingBottom:5,
+    //backgroundColor:"blue",
+  },
+  plus:{
+    height:20,
+    width:20,
+    marginLeft:10,
+    marginRight:10,
+    marginTop:5,
+  },
+
+  arrow:{
+    height:25,
+  },
+
+  textBox: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+    flex: 1,
+    fontSize: 16,
+    paddingHorizontal: 10,
+    height:20,
+  },
+
+  sendButton: {
+    width:40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 15,
+    marginLeft: 5,
+    paddingRight: 15,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+
+  bottom:{
+    height:40,
+  },
+  //MessageBubble
+
+  messageBubble: {
+      margin:0,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      flexDirection:'row',
+      flex: 1,
+  },
+
+
+
+  messageBubbleTD: {
+    borderRadius: 5,
+    marginTop: 8,
+    marginRight: 10,
+    marginLeft: 10,
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+    flexDirection:'column',
+    flex: 1
+  },
+
+
+  messageBubbleLeft: {
+    backgroundColor: '#d5d8d4',
+  },
+
+  messageBubbleTextLeft: {
+    color: 'black',
+    fontSize:16,
+  },
+
+  messageBubbleRight: {
+    backgroundColor: '#66db30'
+  },
+
+  messageBubbleTextRight: {
+    color: 'white',
+    fontSize:16,
+  },
+})
