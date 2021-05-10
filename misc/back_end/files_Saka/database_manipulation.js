4f4dfe4d5a49c769c3a073b4748da039ac0edfbd
K.Sakamoto <mitukouyb@gmail.com>
2021-05-10 20:37:53 +0900

    FOR JIEI

--- misc/back_end/files_Saka/database_manipulation.js
+++ misc/back_end/files_Saka/database_manipulation.js
@@ -1,8 +1,3 @@
-// const mysql = require('mysql');
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected!');
-// });
 
 const mongo = require('mongodb').MongoClient;
 const url = ""; // MongoDB database url
@@ -39,5 +34,66 @@ function post_message(parsed_event) {
         collection.insertOne(JSON.stringify(parsed_event), (error, result) => {
             db.close();
         });
+    });
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password'
+});
+connection.connect((err) => {
+    if(err){
+        console.log("CONNECTION ERROR");
+        return;
+    }
+    console.log("SUCCESSFULLY CONNECTED");
+});
+connection.end((err) => {});
+
+function getuserinfo(userid){
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'user',
+        password: 'password',
+        database: 'info'
+    })
+    con.query('SELECT * FROM info WHERE userid = ?',
+    [userid],
+    (err, rows) => {
+        if(err){
+            return "ERROR";
+        }
+        if(rows.length == 0){
+            return "USER NOT FOUND";
+        }else if(rows.length > 1){
+            return "REGISTRATION ERROR";
+        }else{
+            return rows[0];
+        }
+    });
+}
+
+function verifyid(userid){
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'user',
+        password: 'password',
+        database: 'info'
+    })
+    con.query('SELECT EXISTS(SELECT 1 FROM info WHERE userid = ?',
+    [userid],
+    (err, rows) => {
+        if(err) throw err;
+        if(rows[0] == 0){
+            return "USER NOT FOUND";
+        }
+    });
+    con.query('UPDATE info set isVerified=true WHERE userid = ?',
+    [userid],
+    (err, rows) => {
+        if(err) throw err;
+        return "SUCCESSFULLY VERIFIED";
     });
 }
